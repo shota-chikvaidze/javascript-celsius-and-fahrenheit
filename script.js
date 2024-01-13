2611e3a4d1311d7b197d33af5720dba61c003cc4
shota-chikvaidze <shchikvaidze1@gmail.com>
2024-01-13 16:38:53 +0400

    javascript homework commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let celsius1 = "ცელსიუსი";
+const fahrenheit1 = "ფარენჰეიტი";
+
+document.getElementById("celsius1").innerHTML = celsius1;
+document.getElementById("fahrenheit1").innerHTML = fahrenheit1;
+
+
+
+let celsius2 = 24;
+let fahrenheit2 = (celsius2 * 9/5) + 32;
+
+
+document.getElementById("celsius2").innerHTML = celsius2;
+document.getElementById("fahrenheit2").innerHTML = fahrenheit2;
+
+let celsius3 = 12;
+let fahrenheit3 = (celsius3 * 9/5) + 32;
+
+
+document.getElementById("celsius3").innerHTML = celsius3;
+document.getElementById("fahrenheit3").innerHTML = fahrenheit3;
