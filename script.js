648fa16f1f6a535d1ce2a1a966a355909f7b1f36
shota chikvaidze <129608387+shota-chikvaidze@users.noreply.github.com>
2024-01-13 18:33:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ document.getElementById("fahrenheit1").innerHTML = fahrenheit1;
 
 
 let celsius2 = 24;
-let fahrenheit2 = (celsius2 * 9/5) + 32;
+const fahrenheit2 = (celsius2 * 9/5) + 32;
 
 
 document.getElementById("celsius2").innerHTML = celsius2;
 document.getElementById("fahrenheit2").innerHTML = fahrenheit2;
 
 let celsius3 = 12;
-let fahrenheit3 = (celsius3 * 9/5) + 32;
+const fahrenheit3 = (celsius3 * 9/5) + 32;
 
 
 document.getElementById("celsius3").innerHTML = celsius3;
